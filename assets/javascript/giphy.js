5bd7eb0c98875823d6fa45bafd9d9a9eeac0ad83
LauraAWard <l-ward@northwestern.edu>
2017-10-15 04:09:57 -0500

    10-15 working no css

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,80 +1,108 @@
 $(window).on( "load", function() { //make sure window has finished loading
 
-       var topics = ["pizza", "cupcakes", "milkshake", "broccoli"];
+       var topics = ["undead", "clown", "spider", "monster", "bat", "frankenstein", "coffin", "basement", "tomb", "asylum", 
+       				 "haunted", "graveyard", "alien", "funhouse", "exorcism", "rat", "ghost", "werewolf", "zombie", "raven", 
+       				 "demon", "vampire", "witch", "mummy", "skeleton", "cannibal", "jack-o-lantern", "morgue", "pumpkin"];
 
 
-      function displayGiphy() {
+    function displayGiphy() {
 
         var topicName = $(this).attr("topic-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topicName +  "&api_key=dc6zaTOxFJmzC&limit=10";
- 		console.log(queryURL);
+        var resultCount = 10;
+        var offSet = (Math.floor(Math.random() * (15)) + 1);
+        var apiKey = "dc6zaTOxFJmzC";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topicName + 
+        				"&api_key=" + apiKey + "&limit=" + resultCount + "&offset=" + offSet;
 
- 		for (i = 0; i < 10; i++) {
-         $.ajax({
-          url: queryURL,
-          method: "GET"
+        $.ajax({
+	        url: queryURL,
+	        method: "GET"
         }).done(function(response) {
          
-        console.log(JSON.stringify(response));	
-        var rating = $("<div>");
-
-        var ratingData = response.data.rating;
-        
-        rating.attr("class", "rating");
  
-        rating.html("Rating: " + ratingData);
-        
-        var imageUrl = response.data.images.fixed_height_still;
+	 		for (i = 0; i < resultCount; i++) {
 
- 
-        var topicImage = $("<img>");
+		        var ratingData = response.data[i].rating;
+
+		        var rating = $("<p>");
+		        
+		        rating.attr("class", "rating");
+		        rating.html("Rating: " + ratingData);
+		
+		        $("#images").prepend(rating);
 
 
-        topicImage.attr("src", imageUrl);
-        topicImage.attr("alt", "giphy");
+		        var imageUrlS = response.data[i].images.fixed_height_still.url;
+		        var imageUrlA = response.data[i].images.fixed_height.url;
+		 
+		        var topicImage = $("<img>");
 
+		        topicImage.attr("class", "giphy");
+		        topicImage.attr("src", imageUrlS);
+		        topicImage.attr("alt", (topicName + i));
+		        topicImage.attr("image-still", imageUrlS);
+		        topicImage.attr("image-animate", imageUrlA);
+		        topicImage.attr("image-state", "still");
 
-        $("#images").append(topicImage);
-        $("#images").append(rating);
+		        $("#images").prepend(topicImage);
+		    
+
+		    }    
           
 
         });
-} 
-      }
+ 
+    }
 
 
-      function renderButtons() {
+    function displayButtons() {
 
-         $("#buttons-view").empty();
+        $("#buttons-view").empty();
 
-         for (var i = 0; i < topics.length; i++) {
+        for (var i = 0; i < topics.length; i++) {
 
+	        var topicBtn = $("<button>");
 
-          var topicBtn = $("<button>");
+	        topicBtn.addClass("topic");
 
-          topicBtn.addClass("topic");
+	        topicBtn.attr("topic-name", topics[i]);
 
-          topicBtn.attr("topic-name", topics[i]);
+	        topicBtn.text(topics[i]);
 
-          topicBtn.text(topics[i]);
-
-          $("#buttons-view").append(topicBtn);
+	        $("#buttons-view").append(topicBtn);
         }
-      }
+    }
 
-      $("#add-topic").on("click", function(event) {
+    $("#add-topic").on("click", function(event) {
         event.preventDefault();
 
         var topic = $("#topic-input").val().trim();
 
         topics.push(topic);
 
-        renderButtons();
+        displayButtons();
+
+        $("#topic-input").val("");
 
       });
 
+    function animateGiphy() {
+    
+      	var state = $(this).attr("image-state");
+     	if (state === "still") {
+	        $(this).attr("src", $(this).attr("image-animate"));
+	        $(this).attr("image-state", "animate");
+      } else 
+      	{
+	        $(this).attr("src", $(this).attr("image-still"));
+	        $(this).attr("image-state", "still");
+      	}
+	}
+
       $(document).on("click", ".topic", displayGiphy);
 
-      renderButtons();
+      displayButtons();
+
+      $(document).on("click", ".giphy", animateGiphy);
 
 });
